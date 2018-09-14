767d1b90f91b5064588e3e0eab96a190809ee16f
Christian König <koenig.kersting@gmail.com>
2018-09-14 11:19:26 +0200

    updated project structure and updated code for django2.1

--- otree_manager/dokku_manager/om/urls.py
+++ otree_manager/dokku_manager/om/urls.py
@@ -12,22 +12,24 @@ urlpatterns = [
     path('lobby/<str:instance_name>/<str:participant_label>/', views.lobby, name="lobby"),
     path('lobby/download/<str:instance_name>/<str:os>/', views.download_shortcuts, name="download_shortcuts"),
 
-    path('user/login/', auth_views.login, { 'template_name': 'om/login.html' }, name='login'),
-    path('user/logout/', auth_views.logout, {'template_name': 'om/logout.html' }, name="logout"),
+    path('user/login/', auth_views.LoginView.as_view(template_name='om/login.html'), name='login'),
+    path('user/logout/', auth_views.LogoutView.as_view(template_name='om/logout.html'), name="logout"),
 
     path('user/new/', views.new_user, name='new_user'),
     path('user/list/', views.list_users, name="list_users"),
     path('user/delete/<int:user_id>', views.delete_user, name="delete_user"),
     path('user/edit/<int:user_id>', views.edit_user, name="edit_user"),
     path('user/edit/keyfile/', views.change_key_file, name='change_key_file'),
-    path('user/password/reset/', auth_views.password_reset, {'template_name': 'om/password_reset.html' }, name='password_reset'),
-    path('user/password/reset/done/', auth_views.password_reset_done, {'template_name': 'om/password_reset_done.html' },
+    path('user/password/reset/', auth_views.PasswordResetView.as_view(template_name='om/password_reset.html'), name='password_reset'),
+    path('user/password/reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='om/password_reset_done.html'),
     	name='password_reset_done'),
+    
     re_path(r'^user/password/reset/token/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm, {'template_name': 'om/password_reset_confirm.html' }, name='password_reset_confirm'),
-    path('user/password/reset/done/', auth_views.password_reset_complete, {'template_name': 'om/password_reset_complete.html' }, name='password_reset_complete'),
-    path('user/password/change/', auth_views.password_change, { 'template_name': 'om/password_change.html'}, name='password_change'),
-    path('user/password/change/done/', auth_views.password_change_done, { 'template_name': 'om/password_change_complete.html' }, name='password_change_done'),
+        auth_views.PasswordResetConfirmView.as_view(template_name='om/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('user/password/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='om/password_reset_complete.html'), name='password_reset_complete'),
+    
+    path('user/password/change/', auth_views.PasswordChangeView.as_view(template_name='om/password_change.html'), name='password_change'),
+    path('user/password/change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='om/password_change_complete.html'), name='password_change_done'),
 
 
     path('app/new/', views.new_app, name='new_app'),
