6f969d6daa9e18499f17989534196c4e3d464794
Christian König <koenig.kersting@gmail.com>
2018-10-17 15:09:01 +0200

    demo middleware to disable functions

--- otree_manager/otree_manager/om/middleware.py
+++ otree_manager/otree_manager/om/middleware.py
@@ -4,8 +4,24 @@ class DemoMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         self.disabled_views = [
-            'PasswordResetView',
+            'PasswordChangeDoneView',
+            'PasswordResetDoneView',
+            'PasswordResetConfirmView',
+            'PasswordResetCompleteView',
             'delete_user',
+            'delete',
+        ]
+        self.disabled_post_views = [
+            'PasswordChangeView',
+            'PasswordResetView',
+            'new_user',
+            'change_key_file',
+            'edit_user',
+            'new_app',
+            'change_otree_password',
+            'reset_otree_password',
+            'scale_app',
+            'change_otree_room'
         ]
 
     def __call__(self, request):
@@ -21,9 +37,15 @@ class DemoMiddleware:
 
 
     def process_view(self, request, view_func, view_args, view_kwargs):
+        print('disabled view: %s, method: %s' % (view_func.__name__, request.method))
+        # disable some views completely (i.e. for all request methods)
         if view_func.__name__ in self.disabled_views:
-            print('disabled view: %s' % view_func.__name__)
-            return FeatureDisabled.as_view()
+            return FeatureDisabled.as_view()(request)
+
+        # for some views we only disable POST methods:
+        if view_func.__name__ in self.disabled_post_views:
+            if request.method == "POST":
+                return FeatureDisabled.as_view()(request)
 
 
     def process_template_response(self, request, response):
--- otree_manager/otree_manager/om/views/demo.py
+++ otree_manager/otree_manager/om/views/demo.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
-from django.views.generic.base import TemplateView
+#from django.views.generic.base import TemplateView
+from django.views import View
+from django.shortcuts import render
 
-class FeatureDisabled(TemplateView):
+class FeatureDisabled(View):
     template_name = "om/demo/FeatureDisabled.html"
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     return context
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        return render(request, self.template_name)
